1b23394de230eaf0161144072d93ad88da1720c0
Iván Villarroel <ivillarroelr@inf.udec.cl>
2023-04-22 10:59:56 -0400

    refactor(input_generator.cpp): Modified to generate multiple files

--- informe1/input_generator.cpp
+++ informe1/input_generator.cpp
@@ -10,32 +10,32 @@
  * input <std::string> fileName: name of the output file
  * output void
  */
-void generateInputFileForSortingAlgorithm(unsigned int amountOfRandomNumbers, unsigned int distributionMax, std::string fileName){
-  std::random_device randomDevice;
-  std::mt19937 generator(randomDevice());
-  std::uniform_int_distribution<> distribution(1, distributionMax);
-  std::vector<int> array;
-  for (int i = 0; i < amountOfRandomNumbers; i++) {
-    array.push_back(distribution(generator));
-  }
-  std::ofstream outputFile(fileName);
-  if (outputFile.is_open()) {
-    for (int i = 0; i < array.size(); i++) {
-      outputFile << array[i] << "\n";
+void generateInputFilesForSortingAlgorithm(unsigned int amountOfRandomNumbers, unsigned int distributionMax, unsigned int totalAmountOfFiles){
+  for(int quantity = 1; quantity <= totalAmountOfFiles; quantity++){
+    std::string fileName = "input_" + std::to_string(quantity) + ".txt";
+    std::random_device randomDevice;
+    std::mt19937 generator(randomDevice());
+    std::uniform_int_distribution<> distribution(1, distributionMax);
+    std::vector<int> array;
+    for (int i = 0; i < amountOfRandomNumbers; i++) {
+      array.push_back(distribution(generator));
     }
-    outputFile.close();
-  } else {
-    std::cout << "Error while creating the file" << std::endl;
+    std::ofstream outputFile(fileName);
+    if (outputFile.is_open()) {
+      for (int i = 0; i < array.size(); i++) {
+        outputFile << array[i] << "\n";
+      }
+      outputFile.close();
+    } else {
+      std::cout << "Error while creating the file: "<< fileName << std::endl;
+    }
   }
 }
 
-/**
- * OOB - out of the box input file generator for sorting algorithms
- */
 int main(int argc, char *argv[]) {
   unsigned int amountOfRandomNumbers = atoi(argv[1]);
   unsigned int distributionMax = atoi(argv[2]); 
-  std::string fileName = argv[3];
-  generateInputFileForSortingAlgorithm(amountOfRandomNumbers, distributionMax, fileName);
+  unsigned int totalAmountOfFiles = atoi(argv[3]);
+  generateInputFilesForSortingAlgorithm(amountOfRandomNumbers, distributionMax, totalAmountOfFiles);
   return 0;
 }
