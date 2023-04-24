3d0d5b063ff1816f5d062d11121d2ef5e17dae24
Iván Villarroel <ivillarroelr@inf.udec.cl>
2023-04-23 20:37:01 -0400

    feat(main.cpp): improving performExperiment

--- informe1/source/main.cpp
+++ informe1/source/main.cpp
@@ -88,30 +88,38 @@ void performExperiment(SortingExperiments experiment,
 }
 
 int main(int argc, char *argv[]) {
-  unsigned int amountOfRandomNumbers = atoi(argv[1]);
-  unsigned int distributionMax = atoi(argv[2]);
-  unsigned int totalAmountOfFiles = atoi(argv[3]);
   std::ofstream outputFile(EXPERIMENT_OUTPUT);
   outputFile << "Experiment,Dataset,Time(us)" << std::endl;
-
-  /*
+  if(argc>1){
+    unsigned int amountOfRandomNumbers = atoi(argv[1]);
+    unsigned int distributionMax = atoi(argv[2]);
+    unsigned int totalAmountOfFiles = atoi(argv[3]);
+    /*
    * generates input files from arguments
    * increasing complexity depends on input from command line:
    * amountOfRandomNumbers, distributionMax
    * TODO: add functionality for different characteristics (semi-sorted,
    * partially-sorted)
    */
-  utils.generateInputFilesForSortingAlgorithm(
-      amountOfRandomNumbers, distributionMax, totalAmountOfFiles);
-
+    utils.generateInputFilesForSortingAlgorithm(
+        amountOfRandomNumbers, distributionMax, totalAmountOfFiles);
+  }
   /**
    * Iterates over directory for the input files and then populates the array
    */
   std::filesystem::path input_dir_path =
       std::filesystem::current_path() / "input_files";
 
+  int filecounter = 1;
+  const auto totalFiles =
+      std::distance(std::filesystem::recursive_directory_iterator(input_dir_path),
+                    std::filesystem::recursive_directory_iterator{});
+
   for (const auto &inputFile :
        std::filesystem::directory_iterator(input_dir_path)) {
+
+    int percentage = (filecounter*100)/totalFiles;
+    std::cout << "\r" << "Working: "+std::to_string(percentage)+"%"<< std::flush;
 
     if (inputFile.is_regular_file()) {
 
@@ -130,11 +138,12 @@ int main(int argc, char *argv[]) {
         performExperiment(bubblesort, arrayOfNumbers, outputFile, inputFile.path());
         performExperiment(mergesort, arrayOfNumbers, outputFile, inputFile.path());
         performExperiment(quicksort, arrayOfNumbers, outputFile, inputFile.path());
-
+        filecounter++;
       } else {
         std::cerr << "Error al abrir el archivo: " << inputFile.path() << '\n';
       }
     }
   }
+  std::cout << std::endl;
   return 0;
 }
